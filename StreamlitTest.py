affeaf33e62a18ac5ef4c3e8afd4530fc912236c
Chase Pattee <chasepattee12@gmail.com>
2023-07-04 10:18:14 -0600

    upload streamlit test ui file

--- StreamlitTest.py
+++ StreamlitTest.py
@@ -4,6 +4,10 @@ import duckdb
 
 # set page configurations
 st.set_page_config(layout="centered", page_title='Used Car App')
+st.title('&ensp;&ensp;&ensp;Used Car Price Prediciton App')
+st.text("")
+st.text("")
+st.text("")
 
 conn = duckdb.connect()
 
@@ -12,7 +16,7 @@ listings  = conn.sql('SELECT * FROM clean_training_data.csv').to_df()
 
 # Create Integer Object for Model Year
 
-year_selector = st.number_input(label='Model Year', min_value=1997, max_value=2024)
+year_selector = st.number_input(label='Model Year', value=2023, min_value=1997, max_value=2024)
 
 # Create Select Object for Makes
 col1, col2, col3 = st.columns([1,1,1])
@@ -62,7 +66,7 @@ with col4:
         transmission_selector = st.selectbox(label='Transmission', options=[transmission for transmission in transmissions])
 with col5:
     try:
-        drivetrains = pd.DataFrame(conn.sql(f'''SELECT DISTINCT drivetrain FROM listings WHERE TRIM(make) = '{str(make_selector)}' AND TRIM(model) = '{str(model_selector)}' AND TRIM(trim) LIKE '%{str(trim_selector)}%' AND TRIM(transmission) LIKE '%{str(transmission_selector)}%' AND year = {year_selector} ORDER BY trim''').to_df())
+        drivetrains = pd.DataFrame(conn.sql(f'''SELECT DISTINCT drivetrain FROM listings WHERE TRIM(make) = '{str(make_selector)}' AND TRIM(model) = '{str(model_selector)}' AND year = {year_selector} ORDER BY trim''').to_df())
         drivetrains = pd.Series(drivetrains['drivetrain'])
         drivetrains = drivetrains.tolist()
         drivetrain_selector = st.selectbox(label='Drivetrain', options=[drivetrain for drivetrain in drivetrains])
@@ -76,7 +80,7 @@ st.text("")
 col6, col7, = st.columns([2,2])
 with col6:
     try:
-        exterior_colors = pd.DataFrame(conn.sql(f'''SELECT DISTINCT exterior_color FROM listings WHERE TRIM(make) = '{str(make_selector)}' AND TRIM(model) = '{str(model_selector)}' AND TRIM(trim) LIKE '%{str(trim_selector)}%' AND year = {year_selector} ORDER BY exterior_color''').to_df())
+        exterior_colors = pd.DataFrame(conn.sql(f'''SELECT DISTINCT exterior_color FROM listings WHERE TRIM(make) = '{str(make_selector)}' AND TRIM(model) = '{str(model_selector)}' AND trim LIKE '%{str(trim_selector)}%' AND year = {year_selector} ORDER BY exterior_color''').to_df())
         exterior_colors = pd.Series(exterior_colors['exterior_color'])
         exterior_colors = exterior_colors.tolist()
         ex_color_selector = st.selectbox(label='Exterior Color', options=[ex_color for ex_color in exterior_colors])
@@ -92,37 +96,10 @@ with col7:
         interior_colors = interior_colors.tolist()
         int_color_selector = st.selectbox(label='Interior Color', options=[int_color for int_color in interior_colors])
     except:
-        exterior_colors = pd.DataFrame(conn.sql(f'SELECT DISTINCT exterior_color FROM listings ORDER BY exterior_color').to_df())
-        exterior_colors = pd.Series(exterior_colors['exterior_color'])
-        exterior_colors = exterior_colors.tolist()
-        ex_color_selector = st.selectbox(label='Exterior Color', options=[ex_color for ex_color in exterior_colors])
-st.text("")
-st.text("")
-col6, col7, = st.columns([2,2])
-with col6:
-    try:
-        exterior_colors = pd.DataFrame(conn.sql(f'''SELECT DISTINCT exterior_color FROM listings WHERE TRIM(make) = '{str(make_selector)}' AND TRIM(model) = '{str(model_selector)}' AND TRIM(trim) LIKE '%{str(trim_selector)}%' AND year = {year_selector} ORDER BY exterior_color''').to_df())
-        exterior_colors = pd.Series(exterior_colors['exterior_color'])
-        exterior_colors = exterior_colors.tolist()
-        ex_color_selector = st.selectbox(label='Exterior Color', options=[ex_color for ex_color in exterior_colors])
-    except:
-        exterior_colors = pd.DataFrame(conn.sql(f'SELECT DISTINCT exterior_color FROM listings ORDER BY exterior_color').to_df())
-        exterior_colors = pd.Series(exterior_colors['exterior_color'])
-        exterior_colors = exterior_colors.tolist()
-        ex_color_selector = st.selectbox(label='Exterior Color', options=[ex_color for ex_color in exterior_colors])
-with col7:
-    try:
-        interior_colors = pd.DataFrame(conn.sql(f'''SELECT DISTINCT interior_color FROM listings WHERE TRIM(make) = '{str(make_selector)}' AND TRIM(model) = '{str(model_selector)}' AND TRIM(trim) LIKE '%{str(trim_selector)}%' AND year = {year_selector} ORDER BY interior_color''').to_df())
+        interior_colors = pd.DataFrame(conn.sql(f'SELECT DISTINCT interior_color FROM listings ORDER BY interior_color').to_df())
         interior_colors = pd.Series(interior_colors['interior_color'])
         interior_colors = interior_colors.tolist()
         int_color_selector = st.selectbox(label='Interior Color', options=[int_color for int_color in interior_colors])
-    except:
-        interior_colors = pd.DataFrame(conn.sql(f'SELECT DISTINCT interior_color FROM listings ORDER BY exterior_color').to_df())
-        interior_colors = pd.Series(interior_colors['interior_color'])
-        interior_colors = interior_colors.tolist()
-        int_color_selector = st.selectbox(label='Interior Color', options=[int_color for int_color in interior_colors])
-st.text("")
-st.text("")
 col8, col9, col10 = st.columns([1,0.2,3])
 with col8:
     try:
@@ -134,7 +111,7 @@ with col8:
         states = pd.DataFrame(conn.sql(f'''SELECT DISTINCT state FROM listings ORDER BY state''').to_df())
         states = pd.Series(states['state'])
         states = states.tolist()
-        state_selector = st.selectbox(label='State', options=[state for state in states])
+        state_selector = st.selectbox(label='State', options=[state for state in states], disabled=True)
 with col10:
     try:
         cities = pd.DataFrame(conn.sql(f'''SELECT DISTINCT CASE WHEN state IS NULL THEN NULL ELSE city END city FROM listings WHERE TRIM(state) = '{str(state_selector)}' ORDER BY city''').to_df())
@@ -145,4 +122,4 @@ with col10:
         cities = pd.DataFrame(conn.sql(f'''SELECT DISTINCT CASE WHEN state IS NULL THEN NULL ELSE city END city FROM listings WHERE TRIM(state) = '{str(state_selector)}' ORDER BY city''').to_df())
         cities = pd.Series(cities['city'])
         cities = cities.tolist()
-        city_selector = st.selectbox(label='City', options=[city for city in cities])
+        city_selector = st.selectbox(label='City', options=[city for city in cities], disabled=True)
